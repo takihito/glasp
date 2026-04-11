79f38feecaaf31ba443611aadbb0de9d6184b583
takeda akihito <takeda-akihito@kayac.com>
2026-04-11 16:38:16 +0900

    [tagpr] prepare for the next release

--- cmd/glasp/version.go
+++ cmd/glasp/version.go
@@ -2,7 +2,7 @@ package main
 
 var (
 	// Version is the semantic version. Overridden at build time via ldflags.
-	Version = "v0.2.1"
+	Version = "v0.2.2"
 	// Commit is the git commit hash. Overridden at build time via ldflags.
 	Commit = "none"
 	// Date is the build date. Overridden at build time via ldflags.
